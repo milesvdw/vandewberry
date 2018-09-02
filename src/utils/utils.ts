3c830b17bf787ffaf6966ef28d09e4a4bff625db
Rikki Gibson <rikkigibson@gmail.com>
2018-09-02 15:25:34 -0700

    remove jquery

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,4 @@
 import { Ingredient } from "../models/ingredient";
-import { ajax } from "jquery";
 
 export function fuzzyCompare(str1: string, str2: string, fuzz: string) {
     return str1 === str2 || str1 === str2 + fuzz || str1 + fuzz === str2;
@@ -9,14 +8,6 @@ export function compareIngredients(a: Ingredient, b: Ingredient): boolean {
     let str1 = a.name.replace(' ', '').toLowerCase();
     let str2 = b.name.replace(' ', '').toLowerCase();
     return fuzzyCompare(str1, str2, 's') || fuzzyCompare(str1, str2, 'es') || fuzzyCompare(str1, str2, 'cooked');
-}
-
-export function simpleAjaxCall<T>(url: string): Promise<T> {
-    return ajax({
-        url,
-        method: 'GET',
-        context: document.body
-    }) as any;
 }
 
 // Array.prototype.contains = function (v) {
