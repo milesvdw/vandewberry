28b537ab2118185317f69b1bd50121685aad5418
Miles Van de Wetering <miles.vdw@gmail.com>
2018-09-09 13:04:12 -0700

    added package.zip to gitignore escaped sql queries

--- src/server/inventoryApi.js
+++ src/server/inventoryApi.js
@@ -12,13 +12,16 @@ var post = (pool) => async (req, res) => {
         try {
             connection = pool.getConnection((err2, con) => {
                 con.query(
-                    "UPDATE ingredients SET `name` = '" + req.body.name + "', " +
-                    "category = '" + req.body.category + "', " +
-                    "statusID = " + req.body.statusID + ", " +
-                    "expires = '" + req.body.expires + "', " +
-                    "shelf_life = '" + req.body.shelf_life + "', " +
-                    "householdId = " + req.user.householdId + " WHERE id = " + req.body.id, (err3, ingredient) => {
-                        console.log(err3);
+                    "UPDATE ingredients SET \
+                    `name` = '?', \
+                    category = '?', \
+                    statusID = ?, \
+                    expires = '?', \
+                    shelf_life = '?', \
+                    householdId = ? \
+                    WHERE id = ?",
+                    [req.body.name, req.body.category, req.body.statusId, req.body.expires, req.body.shelf_life, req.body.householdId, req.body.id],
+                    (err3, ingredient) => {
                         res.json(ApiResponse(true, req.body.id));
                     });
             });
@@ -30,13 +33,9 @@ var post = (pool) => async (req, res) => {
         try {
             connection = pool.getConnection((err2, con) => {
                 con.query(
-                    "INSERT INTO ingredients (`name`, category, statusID, expires, shelf_life, householdId) VALUES ( '" +
-                    req.body.name + "', '" +
-                    req.body.category + "', " +
-                    req.body.statusID + ", '" +
-                    req.body.expires + "', '" +
-                    req.body.shelf_life + "', " +
-                    req.user.householdId + ")", (err3, ingredient) => {
+                    "INSERT INTO ingredients (`name`, category, statusID, expires, shelf_life, householdId) VALUES ( '?', '?', ?, '?', '?', ?)",
+                    [req.body.name, req.body.category, req.body.statusId, req.body.expires, req.body.shelf_life, req.body.householdId, req.body.id],
+                     (err3, ingredient) => {
                         con.query("SELECT LAST_INSERT_ID()", (err4, insertResults) => {
                             res.json(ApiResponse(true, insertResults[0]['LAST_INSERT_ID()']));
                         });
@@ -51,7 +50,7 @@ var post = (pool) => async (req, res) => {
 
 var get = (pool) => async (req, res) => {
     try {
-        var ingredients = await pool.query("SELECT ingredients.id as id, name, category, statusID, expires, shelf_life from ingredients WHERE householdId = " + req.user.householdId);
+        var ingredients = await pool.query("SELECT ingredients.id as id, name, category, statusID, expires, shelf_life from ingredients WHERE householdId = ?", [req.user.householdId]);
         res.send(ApiResponse(true, ingredients));
     }
     catch (err) {
