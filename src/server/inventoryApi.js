13c3e7593e09e2b5f5efd21960e729df5b53b988
Miles Van de Wetering <mvandewe@starbucks.com>
2018-09-11 12:29:52 -0700

    refactoring api code (no functional changes)

--- src/server/inventoryApi.js
+++ src/server/inventoryApi.js
@@ -12,6 +12,99 @@ var deleteItem = (pool) => async (req, res) => {
     }
 }
 
+async function getExistingIngredientById(id) {
+    await pool.query("SELECT ingredients.*, ingredientgroups.name as name FROM ingredients \
+            INNER JOIN ingredientgroups \
+            ON ingredients.id = ? AND ingredients.ingredientGroupId = ingredientgroups.id", [id]);
+}
+
+async function updateIngredientValues(category, statusID, expires, shelf_life, id, ingredientGroupId) {
+    if (ingredientGroupid) {
+
+        await pool.query("UPDATE ingredients SET \
+                        ingredientGroupId = ?, \
+                        category = ?, \
+                        statusID = ?, \
+                        expires = ?, \
+                        shelf_life = ? \
+                        WHERE id = ?",
+            [ingredientGroupId, category, statusID, expires, shelf_life, id]);
+    }
+    else {
+
+    }
+}
+
+async function getIngredientGroupbyName(name) {
+    let existingGroups = await pool.query("SELECT * FROM ingredientgroups WHERE `name` = ? LIMIT 1", [name]);
+    return existingGroups;
+}
+
+async function updateIngredient(user, ingredient) {
+    var existingIngredient = await getExistingIngredientByid(ingredient.id)
+    if (existingIngredient.length === 0) {
+        console.log("ERROR: tried to update a non-existent ingredient");
+        return;
+    }
+    if (existingIngredient[0].householdId !== user.householdId) {
+        console.log("ERROR: tried to update another user's ingredient");
+        return;
+    }
+
+    var ingredientGroupId;
+    if (existingIngredient[0].name.trim() !== ingredient.name.trim()) {
+        // decouple this from the old ingredient group and potentially create a new ingredient group for it
+        let existingGroups = await getIngredientGroupbyName(ingredient.name.trim());
+
+        if (existingGroups.length > 0) {
+            ingredientGroupId = existingGroups[0].id;
+        } else {
+            await pool.query("INSERT INTO ingredientgroups (`name`) VALUES (?)", [ingredient.name.trim()]);
+            ingredientGroupId = (await getIngredientGroupbyName(ingredient.name.trim()))[0].id;
+        }
+
+        await updateIngredientValues(ingredient.category, ingredient.statusID, ingredient.expires ? 1 : 0, ingredient.shelf_life, ingredient.id, insertedIngredientGroupId);
+    } else {
+        // just update the ingredient's fields
+        await updateIngredientValues(ingredient.category, ingredient.statusID, ingredient.expires ? 1 : 0, ingredient.shelf_life, ingredient.id)
+
+    }
+    res.json(ApiResponse(true, ingredient.id));
+}
+
+async function insertIngredientValues(ingredientGroupId, category, statusID, expires, shelf_life, householdId) {
+    pool.getConnection((err, con) => {
+        con.query("INSERT INTO ingredients (`ingredientGroupId`, `category`, `statusID`, `expires`, `shelf_life`, `householdId`) \
+            VALUES (?, ?, ?, ?, ?, ?) ",
+            [ingredientGroupId, category, statusID, expires, shelf_life, householdId], (err2, ignore) => {
+                con.query("SELECT LAST_INSERT_ID()", (err3, insertedIdRaw) => {
+                    if (err3) {
+                        console.log("ERROR at selecting last insert id after inserting new ingredient");
+                        console.log(err3);
+                        con.release();
+                        res.json(ApiResponse(true, false));
+                        return;
+                    }
+                    con.release();
+                    res.json(ApiResponse(true, ingredient.id));
+                })
+            });
+    });
+}
+
+async function insertIngredient(user, ingredient) {
+    var ingredientGroupId;
+
+    let existingGroups = await getIngredientGroupbyName(ingredient.name.trim());
+    if (existingGroups.length > 0) {
+        ingredientGroupId = existingGroups[0].id;
+    } else {
+        await pool.query("INSERT INTO ingredientgroups (`name`) VALUES (?)", [ingredient.name.trim()]);
+        ingredientGroupId = (await getIngredientGroupbyName(ingredient.name.trim()))[0].id;
+    }
+    return await insertIngredientValues(ingredientGroupId, ingredient.category, ingredient.statusID, ingredient.expires ? 1 : 0, ingredient.shelf_life, user.householdId);
+}
+
 var post = (pool) => async (req, res) => {
     if (req.body.name === "") {
         console.log("ERROR: damn users making empty ingredients");
@@ -19,114 +112,16 @@ var post = (pool) => async (req, res) => {
     }
     if (req.body.id > 0) {
         try {
-            var existingIngredient = await pool.query("SELECT ingredients.*, ingredientgroups.name as name FROM ingredients \
-            INNER JOIN ingredientgroups \
-            ON ingredients.id = ? AND ingredients.ingredientGroupId = ingredientgroups.id", [req.body.id]);
-            if (existingIngredient.length === 0) {
-                console.log("ERROR: tried to update a non-existent ingredient");
-                return;
-            }
-            if (existingIngredient[0].householdId !== req.user.householdId) {
-                console.log("ERROR: tried to update another user's ingredient");
-                return;
-            }
-
-            if (existingIngredient[0].name.trim() !== req.body.name.trim()) {
-                // decouple this from the old ingredient group and potentially create a new ingredient group for it
-                let existingGroups = await pool.query("SELECT * FROM ingredientgroups WHERE `name` = ?", [req.body.name.trim()]);
-                if (existingGroups.length > 0) {
-                    // hook the material up to the existing ingredientgroup
-                    await pool.query("INSERT INTO ingredientgroups (`name`) VALUES (?)", [req.body.name.trim()]); // TODO consider optimizing this to use one connection
-                    let insertedIngredientGroupId = (await pool.query("SELECT * from ingredientgroups WHERE `name` = ? LIMIT 1", [req.body.name.trim()]))[0].id;
-                    // just update the ingredient's fields
-                    await pool.query("UPDATE ingredients SET \
-                        ingredientGroupId = ?, \
-                        category = ?, \
-                        statusID = ?, \
-                        expires = ?, \
-                        shelf_life = ? \
-                        WHERE id = ?",
-                        [insertedIngredientGroupId, req.body.category, req.body.statusID, req.body.expires ? 1 : 0, req.body.shelf_life, req.body.id]);
-                    res.json(ApiResponse(true, req.body.id));
-                    return;
-                } else {
-                    await pool.query("INSERT INTO ingredientgroups (`name`) VALUES (?)", [req.body.name.trim()]);
-                    let insertedIngredientGroupId = (await pool.query("SELECT * from ingredientgroups WHERE `name` = ? LIMIT 1", [req.body.name.trim()]))[0].id;
-                    await pool.query("UPDATE ingredients SET \
-                        ingredientGroupId = ?, \
-                        category = ?, \
-                        statusID = ?, \
-                        expires = ?, \
-                        shelf_life = ? \
-                        WHERE id = ?",
-                        [insertedIngredientGroupId, req.body.category, req.body.statusID, req.body.expires ? 1 : 0, req.body.shelf_life, req.body.id]);
-                    res.json(ApiResponse(true, req.body.id));
-                    return;
-                }
-            } else {
-                // just update the ingredient's fields
-                await pool.query("UPDATE ingredients SET \
-                    category = ?, \
-                    statusID = ?, \
-                    expires = ?, \
-                    shelf_life = ? \
-                    WHERE id = ?",
-                    [req.body.category, req.body.statusID, req.body.expires ? 1 : 0, req.body.shelf_life, req.body.id]);
-                res.json(ApiResponse(true, req.body.id));
-                return;
-            }
+            return await updateIngredient(req.user, req.body);
         }
         catch (err) {
-            res.send(ApiResponse(true, []));
+            res.send(ApiResponse(true, false));
             return;
         }
     } else {
         // create a brand-new ingredient
         try {
-            let existingGroups = await pool.query("SELECT * FROM ingredientgroups WHERE `name` = ?", [req.body.name]);
-            if (existingGroups.length > 0) {
-                // hook the material up to the existing ingredientgroup
-                await pool.query("INSERT INTO ingredientgroups (`name`) VALUES (?)", [req.body.name]); // TODO consider optimizing this to use one connection
-                let insertedIngredientGroupId = (await pool.query("SELECT * from ingredientgroups WHERE `name` = ? LIMIT 1", [req.body.name.trim()]))[0].id;
-                // just update the ingredient's fields
-                pool.getConnection((err, con) => {
-                    con.query("INSERT INTO ingredients (`ingredientGroupId`, `category`, `statusID`, `expires`, `shelf_life`, `householdId`) \
-                    VALUES (?, ?, ?, ?, ?, ?) ",
-                        [insertedIngredientGroupId, req.body.category, req.body.statusID, req.body.expires ? 1 : 0, req.body.shelf_life, req.user.householdId], (err2, ignore) => {
-                            con.query("SELECT LAST_INSERT_ID()", (err3, insertedIdRaw) => {
-                                if (err3) {
-                                    console.log("ERROR at selecting last insert id after inserting new ingredient");
-                                    console.log(err3);
-                                    con.release();
-                                    res.json(ApiResponse(true, false));
-                                    return;
-                                }
-                                con.release();
-                                res.json(ApiResponse(true, req.body.id));
-                            })
-                        });
-                });
-            } else {
-                await pool.query("INSERT INTO ingredientgroups (`name`) VALUES (?)", [req.body.name.trim()]);
-                let insertedIngredientGroupId = (await pool.query("SELECT * from ingredientgroups WHERE `name` = ? LIMIT 1", [req.body.name.trim()]))[0].id;
-                pool.getConnection((err, con) => {
-                    con.query("INSERT INTO ingredients (`ingredientGroupId`, `category`, `statusID`, `expires`, `shelf_life`, `householdId`) \
-                        VALUES (?, ?, ?, ?, ?, ?) ",
-                        [insertedIngredientGroupId, req.body.category, req.body.statusID, req.body.expires ? 1 : 0, req.body.shelf_life, req.user.householdId], (err2, ignore) => {
-                            con.query("SELECT LAST_INSERT_ID()", (err3, insertedIdRaw) => {
-                                if (err3) {
-                                    console.log("ERROR at selecting last insert id after inserting new ingredient");
-                                    console.log(err3);
-                                    con.release();
-                                    res.json(ApiResponse(true, false));
-                                    return;
-                                }
-                                con.release();
-                                res.json(ApiResponse(true, req.body.id));
-                            })
-                        });
-                });
-            }
+            return await insertIngredient(req.user, req.body)
         }
         catch (err) {
             res.send(ApiResponse(true, false));
