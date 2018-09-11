a8c6679a1af02c6e66889e1fa0fb029b82a044f6
Miles Van de Wetering <miles.vdw@gmail.com>
2018-09-11 16:40:08 -0700

    Refactor from recursive garbage functions to nice promises

--- src/server/recipesApi.js
+++ src/server/recipesApi.js
@@ -6,7 +6,7 @@ const ApiResponse = require('./apiResponse').ApiResponse
 
 var deleteRecipe = (pool) => async (req, res) => {
     var recipes = (await pool.query("SELECT * FROM recipes WHERE id = ?", [req.body.id]));
-    if(recipes.length === 0) {
+    if (!QueryHadResults(recipes)) {
         console.log("ERROR: Can't delete a recipe that doesn't exist");
         res.json(ApiResponse(true, false));
         return;
@@ -33,10 +33,10 @@ var deleteRecipe = (pool) => async (req, res) => {
 
 async function clearOldMaterials(pool, recipeId) {
     var preexistingMaterials = await pool.query("SELECT * FROM materials WHERE `recipeId` = ?", [recipeId]);
-    if (preexistingMaterials.length > 0) {
+    if (QueryHadResults(preexistingMaterials)) {
         var preexistingMaterialIds = preexistingMaterials.map((row) => row.id);
         await pool.query("DELETE FROM materials_ingredientgroups WHERE `materialId` IN (?)", [preexistingMaterialIds]); // delete old materials from this recipe so we can save fresh
-        await pool.query("DELETE FROM materials WHERE `materialId` IN (?)", [preexistingMaterialIds]);
+        await pool.query("DELETE FROM materials WHERE `id` IN (?)", [preexistingMaterialIds]);
     }
 }
 
@@ -64,16 +64,25 @@ async function createRecipe(req, pool, con, res) {
 
                 await clearOldMaterials(pool, recipeId);
 
-                insert_update_materials(pool, req, recipeId, 0, () => {
-                    res.json(ApiResponse(true, recipeId))
-                })
+                var promises = req.body.materials.map((mat) => {
+                    return insertUpdateMaterials(mat, pool, recipeId);
+                });
+                await Promise.all(promises);
+                res.json(ApiResponse(true, req.body.id))
             });
         });
 };
 
+
+function QueryHadResults(query) {
+    return query.length > 0;
+}
+
 async function updateRecipe(req, pool, con, res) {
+
     var existingRecipe = await pool.query("SELECT * FROM recipes WHERE id = ?", [req.body.id]);
-    if (existingRecipe.length > 0) {
+
+    if (QueryHadResults(existingRecipe)) {
         if (existingRecipe[0].householdId !== req.user.householdId) {
             console.log("ERROR user attempted to update a recipe that their household doesn't own");
             con.release();
@@ -84,78 +93,60 @@ async function updateRecipe(req, pool, con, res) {
         con.release();
         return;
     }
+
     con.query("UPDATE recipes SET `name` = ?, `description` = ?, `calories` = ?, `lastEaten` = ? WHERE `id` = ?",
         [req.body.name, req.body.description, req.body.calories, req.body.lastEaten, req.body.id],
-        (err, results) => {
+        async (err, results) => {
             if (err) {
                 console.log("ERROR while updating existing recipe");
                 console.log(err);
                 con.release();
             }
-            
-            await clearOldMaterials(pool, recipeId);
-            
-            insert_update_materials(pool, req, req.body.id, 0, () => {
-                res.json(ApiResponse(true, req.body.id))
-            })
+
+            await clearOldMaterials(pool, req.body.id);
+
+            var promises = req.body.materials.map((mat) => {
+                return insertUpdateMaterials(mat, pool, req.body.id);
+            });
+            await Promise.all(promises);
+            res.json(ApiResponse(true, req.body.id))
 
         })
 }
 
-async function insert_update_ingredientGroups(pool, material, index, cb, groupIds = []) {
-    if (index === material.ingredientgroups.length) {
-        cb(groupIds); // no ingredientgroups left to add
-        return;
-    }
+async function insertUpdateIngredientGroups(ingredientgroup, pool) {
 
-    var ingredientGroup = material.ingredientgroups[index];
-    if (ingredientGroup.name !== "") {
-        let existingGroups = await pool.query("SELECT * FROM ingredientgroups WHERE `name` = ?", [ingredientGroup.name]);
-        if (existingGroups.length > 0) {
+    if (ingredientgroup.name !== "") {
+        let existingGroups = await pool.query("SELECT * FROM ingredientgroups WHERE `name` = ?", [ingredientgroup.name]);
+        if (QueryHadResults(existingGroups)) {
             // hook the material up to the existing ingredientgroup
-            groupIds.push(existingGroups[0].id);
-            return insert_update_ingredientGroups(pool, material, index + 1, cb, groupIds);
+            return existingGroups[0].id;
         } else {
-            await pool.query("INSERT INTO ingredientgroups (`name`) VALUES (?)", [ingredientGroup.name]);
-            existingGroups = await pool.query("SELECT * FROM ingredientgroups WHERE `name` = ?", [ingredientGroup.name]);
-            groupIds.push(existingGroups[0].id);
-            return insert_update_ingredientGroups(pool, material, index + 1, cb, groupIds);
+            await pool.query("INSERT INTO ingredientgroups (`name`) VALUES (?)", [ingredientgroup.name]);
+            existingGroups = await pool.query("SELECT * FROM ingredientgroups WHERE `name` = ?", [ingredientgroup.name]);
+            return existingGroups[0].id;
         }
-
     }
-    insert_update_ingredientGroups(pool, material, index + 1, cb, groupIds);
+    return; // NOTE: this may cause a bug depending on how returning null is handled by promise.all
 }
 
-async function link_material_ingredientGroups(groupIds, materialId, con) {
-    if (groupIds.length > 0) {
-        let groupId = groupIds.pop();
-        con.query("INSERT INTO materials_ingredientgroups (`materialId`, `ingredientGroupId`) VALUES (?, ?)", [materialId, groupId], (err, ignore) => {
-            if (err) {
-                console.log("ERROR while linking new material to ingredientgroup");
-                console.log(err);
-                con.release();
-                return;
-            } else {
-                link_material_ingredientGroups(groupIds, materialId, con);
-            }
-        })
-    } else {
+async function linkMaterialIngredientGroups(gid, materialId, con) {
+    con.query("INSERT INTO materials_ingredientgroups (`materialId`, `ingredientGroupId`) VALUES (?, ?)", [materialId, gid], (err, ignore) => {
+        if (err) {
+            console.log("ERROR while linking new material to ingredientgroup");
+            console.log(err);
+            con.release();
+            return;
+        }
         con.release();
-        return;
-    }
+    })
 }
 
-async function insert_update_materials(pool, req, recipeId, index, cb) {
-    if (index === req.body.materials.length) {
-        cb();
-        return;
-    }
-    var material = req.body.materials[index];
-
+async function insertUpdateMaterials(material, pool, recipeId) {
     var hasIngredients = material.ingredientgroups.length > 0 && !!material.ingredientgroups[0].name;
-    if(!hasIngredients) {
+    if (!hasIngredients) {
         console.log("WARNING: user tried to create a material with no ingredients");
-        return insert_update_materials(pool, req, recipeId, index + 1, cb);
+        return;
     }
 
     // first insert the ingredient groups
@@ -164,36 +155,45 @@ async function insert_update_materials(pool, req, recipeId, index, cb) {
         await pool.query("DELETE FROM materials_ingredientgroups WHERE materialId = ?", [material.id]);
         await pool.query("DELETE FROM materials WHERE id = ?", [material.id]);
     }
-    insert_update_ingredientGroups(pool, material, 0, (groupIds) => {
-        if (groupIds.length === 0) {
-            return; // no ingredients to link this material to...
+
+    var promises = material.ingredientgroups.map((ig) => {
+        return insertUpdateIngredientGroups(ig, pool)
+    })
+
+    var groupIds = await Promise.all(promises);
+
+    if (groupIds.length === 0) {
+        return; // no ingredients to link this material to...
+    }
+    pool.getConnection((err, con) => {
+        if (err) {
+            console.log("ERROR: getting connection to insert materials");
+            console.log(err);
+            return;
         }
-        pool.getConnection((err, con) => {
-            if (err) {
-                console.log("ERROR: getting connection to insert materials");
-                console.log(err);
+        con.query("INSERT INTO materials (`recipeId`, `quantity`, `required`) VALUES (?, ?, ?)", [recipeId, material.quantity, material.required ? 1 : 0], (err2, ignore) => {
+            if (err2) {
+                console.log("ERROR while inserting new material");
+                console.log(err2);
+                con.release();
                 return;
             }
-            con.query("INSERT INTO materials (`recipeId`, `quantity`, `required`) VALUES (?, ?, ?)", [recipeId, material.quantity, material.required ? 1 : 0], (err2, ignore) => {
-                if (err2) {
-                    console.log("ERROR while inserting new material");
-                    console.log(err2);
+            con.query("SELECT LAST_INSERT_ID()", async (err3, insertedIdRaw) => {
+                if (err3) {
+                    console.log("ERROR at selecting last insert id after inserting new material");
+                    console.log(err3);
                     con.release();
                     return;
                 }
-                con.query("SELECT LAST_INSERT_ID()", (err3, insertedIdRaw) => {
-                    if (err3) {
-                        console.log("ERROR at selecting last insert id after inserting new material");
-                        console.log(err3);
-                        con.release();
-                        return;
-                    }
-                    link_material_ingredientGroups(groupIds, insertedIdRaw[0]['LAST_INSERT_ID()'], con)
-                })
+                var materialId = insertedIdRaw[0]['LAST_INSERT_ID()']
+                await Promise.all(groupIds.map((gid) => {
+                    return linkMaterialIngredientGroups(gid, materialId, con)
+                }))
             })
-        });
+        })
     });
-    return insert_update_materials(pool, req, recipeId, index + 1, cb);
+
+    return;
 };
 
 
@@ -286,7 +286,7 @@ var get = (pool) => async (req, res) => {
     try {
         var recipeIds = (await pool.query("SELECT * FROM (SELECT recipes.id as id from recipes where householdId = ?) as r \
          UNION (SELECT recipeId as id from household_recipes where householdId = ?)", [req.user.householdId, req.user.householdId])).map((rrow) => rrow.id);
-        if(recipeIds.length === 0) {
+        if (recipeIds.length === 0) {
             res.send(ApiResponse(true, []));
             return;
         }
