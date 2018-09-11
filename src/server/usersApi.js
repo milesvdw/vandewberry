80b60bbcf24197a8220eaf3bfda530f68fbc4d25
Miles Van de Wetering <miles.vdw@gmail.com>
2018-09-10 18:45:09 -0700

    Fixes to inventoryApi; should be working fully now

--- src/server/usersApi.js
+++ src/server/usersApi.js
@@ -12,7 +12,6 @@ var createAccount = (pool) => async (req, res) => {
 
     try {
         var users = await pool.query("SELECT FROM users WHERE username = ?", [req.body.username]);
-        // db.collection('users').find({ user: req.body.username }).toArray((geterr, items) => {
         if (users.length > 0) {
             res.send(ApiResponse(false, false));
             return;
@@ -20,9 +19,7 @@ var createAccount = (pool) => async (req, res) => {
         var households = await pool.query("SELECT * FROM households WHERE `name` = ?", [req.body.household]);
 
         await pool.query("INSERT INTO users (`username`, `password`, `householdId`) VALUES ( ?, ?, ?)", [req.body.username, hash, households[0].id]);
-        // db.collection('users').save({ user: req.body.username, passwordHash: hash, household: req.body.household });
         res.send(ApiResponse(false, true));
-        // });
     }
     catch (err) {
         res.send(ApiResponse(false, null));
