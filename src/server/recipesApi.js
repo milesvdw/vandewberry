999adbd9ff08cff449bc283bae3f2580f1a7b6d5
Miles Van de Wetering <miles.vdw@gmail.com>
2018-09-11 18:44:16 -0700

    Refactoring functions to repo file

--- src/server/recipesApi.js
+++ src/server/recipesApi.js
@@ -2,6 +2,7 @@
 /* tslint:disable:no-string-literal */
 var mysql = require('mysql');
 const ApiResponse = require('./apiResponse').ApiResponse
+const Repo = require('./repo');
 // const utils = require('../utils/utils')
 
 var deleteRecipe = (pool) => async (req, res) => {
@@ -40,10 +41,10 @@ async function clearOldMaterials(pool, recipeId) {
     }
 }
 
-async function createRecipe(req, pool, con, res) {
+async function createRecipe(recipe, pool, con, res) {
     con.query("INSERT INTO recipes (`name`, `description`, `calories`, `lastEaten`, `householdId`) \
     VALUES (?, ?, ?, ?, ?)",
-        [req.body.name, req.body.description, req.body.calories, req.body.lastEaten, req.user.householdId],
+        [recipe.name, recipe.description, recipe.calories, recipe.lastEaten, recipe.householdId],
         (err2, ignore) => {
             if (err2) {
                 console.log("ERROR while inserting recipe");
@@ -64,14 +65,14 @@ async function createRecipe(req, pool, con, res) {
 
                 await clearOldMaterials(pool, recipeId);
 
-                var promises = req.body.materials.map((mat) => {
+                var promises = recipe.materials.map((mat) => {
                     return insertUpdateMaterials(mat, pool, recipeId);
                 });
                 await Promise.all(promises);
-                res.json(ApiResponse(true, recipeId))
+                return ApiResponse(true, recipeId)
             });
         });
-};
+}
 
 
 function QueryHadResults(query) {
@@ -130,75 +131,9 @@ async function insertUpdateIngredientGroups(ingredientgroup, pool) {
     return; // NOTE: this may cause a bug depending on how returning null is handled by promise.all
 }
 
-async function linkMaterialIngredientGroups(gid, materialId, con) {
-    con.query("INSERT INTO materials_ingredientgroups (`materialId`, `ingredientGroupId`) VALUES (?, ?)", [materialId, gid], (err, ignore) => {
-        if (err) {
-            console.log("ERROR while linking new material to ingredientgroup");
-            console.log(err);
-            con.release();
-            return;
-        }
-    })
-}
-
-async function insertUpdateMaterials(material, pool, recipeId) {
-    var hasIngredients = material.ingredientgroups.length > 0 && !!material.ingredientgroups[0].name;
-    if (!hasIngredients) {
-        console.log("WARNING: user tried to create a material with no ingredients");
-        return;
-    }
-
-    // first insert the ingredient groups
-    if (material.id > 0) {
-        // drop existing material_ingredientgroup connections
-        await pool.query("DELETE FROM materials_ingredientgroups WHERE materialId = ?", [material.id]);
-        await pool.query("DELETE FROM materials WHERE id = ?", [material.id]);
-    }
-
-    var promises = material.ingredientgroups.map((ig) => {
-        return insertUpdateIngredientGroups(ig, pool)
-    })
-
-    var groupIds = await Promise.all(promises);
-
-    if (groupIds.length === 0) {
-        return; // no ingredients to link this material to...
-    }
-    pool.getConnection((err, con) => {
-        if (err) {
-            console.log("ERROR: getting connection to insert materials");
-            console.log(err);
-            return;
-        }
-        con.query("INSERT INTO materials (`recipeId`, `quantity`, `required`) VALUES (?, ?, ?)", [recipeId, material.quantity, material.required ? 1 : 0], (err2, ignore) => {
-            if (err2) {
-                console.log("ERROR while inserting new material");
-                console.log(err2);
-                con.release();
-                return;
-            }
-            con.query("SELECT LAST_INSERT_ID()", async (err3, insertedIdRaw) => {
-                if (err3) {
-                    console.log("ERROR at selecting last insert id after inserting new material");
-                    console.log(err3);
-                    con.release();
-                    return;
-                }
-                var materialId = insertedIdRaw[0]['LAST_INSERT_ID()']
-                await Promise.all(groupIds.map((gid) => {
-                    return linkMaterialIngredientGroups(gid, materialId, con)
-                }))
-                con.release();
-            })
-        })
-    });
-
-    return;
-};
-
-
 var post = (pool) => async (req, res) => {
-    if(!req.body.name || req.body.name.length < 1) {
+    req.body.householdId = req.user.householdId
+    if (!req.body.name || req.body.name.length < 1) {
         console.log("User tried to save a recipe with an empty name. Shame on them, their family, and their dog.");
         res.json(ApiResponse(true, false));
         return;
@@ -206,13 +141,13 @@ var post = (pool) => async (req, res) => {
     if (req.body.id > 0) {
         // do an update
         pool.getConnection((err, con) => {
-            updateRecipe(req, pool, con, res);
+            Repo.updateRecipe(req, pool, con, res);
         });
     }
     else {
         // do an insert
-        pool.getConnection((err, con) => {
-            createRecipe(req, pool, con, res);
+        pool.getConnection(async (err, con) => {
+            res.json(await Repo.createRecipe(req.body, pool, con, res));
         });
     }
 }
@@ -232,47 +167,6 @@ var share = (pool) => async (req, res) => {
     res.json(ApiResponse(true, req.body.id));
 }
 
-function constructRecipeFromRows(rows) {
-    //expects a list of rows, with each row having one unique recipe-material-ingredientgroup-ingredient combination
-    recipe = {};
-    recipe.id = rows[0].recipeId;
-    recipe.description = rows[0].recipeDescription // NOTE: I'm probably sending over duplicate descriptions which could get expensive idk
-    recipe.name = rows[0].recipeName
-    recipe.calories = rows[0].recipeCalories
-    recipe.lastEaten = rows[0].lastEaten
-    recipe.householdId = rows[0].householdId
-
-    let materialIds = rows.map((r) => r.materialId).unique(); // non-unique list of recipe ids
-    recipe.materials = []
-    materialIds.forEach((id) =>
-        recipe.materials.push(constructMaterialFromRows(rows.filter((row) =>
-            row.materialId === id))))
-    return recipe;
-}
-
-function constructMaterialFromRows(rows) {
-    material = {}
-    material.id = rows[0].materialId
-    material.quantity = rows[0].materialQuantity
-    material.required = rows[0].materialRequired
-    let ingredientgroupIds = rows.map((r) => r.ingredientGroupId) // non-unique list of recipe ids
-    ingredientgroupIds = ingredientgroupIds.unique();
-
-    material.ingredientgroups = []
-    ingredientgroupIds.forEach((id) =>
-        material.ingredientgroups.push(constructIngredientGroupFromRows(rows.filter((row) =>
-            row.ingredientGroupId === id))))
-    return material;
-}
-
-function constructIngredientGroupFromRows(rows) {
-    ingredientGroup = {};
-    ingredientGroup.id = rows[0].ingredientGroupId;
-    ingredientGroup.name = rows[0].ingredientGroupName;
-
-    return ingredientGroup;
-}
-
 Array.prototype.unique = function () {
     return this.filter((value, index) => this.indexOf(value) === index);
 }
@@ -286,28 +180,7 @@ var get = (pool) => async (req, res) => {
             return;
         }
 
-        var sqlRecipes = await pool.query("SELECT recipes.id AS recipeId, \
-        recipes.description AS recipeDescription, \
-        recipes.name AS recipeName, \
-        recipes.calories AS recipeCalories, \
-        recipes.lastEaten AS recipeLastEaten, \
-        recipes.householdId AS householdId, \
-        materials.id AS materialId, \
-        materials.quantity AS materialQuantity, \
-        materials.required AS materialRequired, \
-        ingredientgroups.id AS ingredientGroupId, \
-        ingredientgroups.name AS ingredientGroupName \
-        FROM recipes \
-        LEFT JOIN materials ON materials.recipeId = recipes.id \
-        LEFT JOIN materials_ingredientgroups ON materials_ingredientgroups.materialId = materials.id \
-        LEFT JOIN ingredientgroups ON materials_ingredientgroups.ingredientGroupId = ingredientgroups.id \
-        WHERE recipes.id IN (?)", [recipeIds]);
-
-        let finalRecipes = [];
-        recipeIds.forEach((rId) => {
-            finalRecipes.push(constructRecipeFromRows(sqlRecipes.filter((row) => row['recipeId'] === rId))) // // TODO: this results in duplicate work as we filter this list several times, should be optimized 
-        })
-        res.send(ApiResponse(true, finalRecipes));
+        return res.send(await Repo.getRecipesByIdList(recipeIds, pool));
     }
     catch (err) {
         console.log(err);
